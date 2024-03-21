61e65f6bdab8174dcfa83431f36997aa36d8e469
Amit kumar <amitkumar.it@yatrirestro.com>
2024-03-21 14:01:22 +0530

    project start and endpoints added

---
+++ config/endPoints.js
@@ -0,0 +1,16 @@
+export const endpoints = {
+    user:{
+        login:"api/user/login",
+        signup:"api/user/signup",
+        me:"api/user",
+        find:"api/user", // api/user/:name;
+        userdetails:"api/user/all", // api/user/all?username=username;
+        searchUser:"api/user", // api/user/:name
+        follow:"api/user/addfollowing", // post request provid followingId
+        unfollow:""
+    },
+    blog:{
+        postBlog:"",
+        suggestBlog:"",
+    }
+}
